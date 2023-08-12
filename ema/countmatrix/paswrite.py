aa7d4029c9dd40729997940fa64fd56086e93c16
Trex <amiramiritabat01@gmail.com>
2023-08-12 20:51:19 +0300

    complete paswrite function

---
+++ ema/countmatrix/paswrite.py
@@ -0,0 +1,21 @@
+from indexing import indexing
+
+direction_dict = {True:1, False:-1}
+def pas_write(chro:int, peak_start:int, l_end:int, strand:bool, output):
+    pas_list = [peak_start, l_end][::-direction_dict[strand]]
+    peak_bed = f"{chro}\t{pas_list[0]}\t{pas_list[1]}\n"
+    output.write(peak_bed)
+
+
+def matrix_write(cb_dict:dict, pasnumber:int, output): 
+    '''
+    cb_dict: Peak object attribute that contain CellBarcodes of peak class
+    pasnumber: Peak class level attribute
+    output: Path to out put file 
+
+    Write count matrix in MatrixMarket Format
+    '''
+    for cb in cb_dict:
+        col, count = indexing(cb), cb_dict[cb]
+        matx_str = f"{pasnumber} {col} {count}\n"
+        output.write(matx_str)
