1042b11595a4f78fc5afad191669c996f04aa9df
Trex <amiramiritabat01@gmail.com>
2023-08-20 18:43:46 +0300

    fix __init__.py

--- ema/__init__.py
+++ ema/__init__.py
@@ -4,4 +4,6 @@
 from .countmatrix.peackcalling import peak_calling
 from .countmatrix.indexing import indexing, cb_index
 from .countmatrix.paswrite import pas_write, matrix_write
-from .countmatrix.
+from .countmatrix.peak import Peak
+from .countmatrix.read import read_check
+from .config import directory_config, variable_config, filter_config
