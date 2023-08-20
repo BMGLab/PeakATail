d6c806caf98bf0c89a5d1ae08f3a602173e81169
Trex <amiramiritabat01@gmail.com>
2023-08-20 18:41:43 +0300

    add importing modules to init

---
+++ ema/__init__.py
@@ -0,0 +1,7 @@
+
+
+
+from .countmatrix.peackcalling import peak_calling
+from .countmatrix.indexing import indexing, cb_index
+from .countmatrix.paswrite import pas_write, matrix_write
+from .countmatrix.
