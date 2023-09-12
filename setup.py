380a58577c6a1a8869db7b5b4a0108930ed44579
Trex <amiramiritabat01@gmail.com>
2023-09-12 11:29:40 +0300

    add igraph and louvain to dependencies and make versions constant

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ setup(
     name="ema",
     version='0.1.a1',
     packages=find_packages(),
-    install_requires=["pysam>=0.17.0", 
-                      "anndata>=0.9.1", 
-                      "scanpy>=1.9.3", 
-                      "pandas>=1.3.5", 
-                      "scipy>=1.8.0"],
+    install_requires=["pysam=0.17.0", 
+                      "anndata=0.9.1", 
+                      "scanpy=1.9.3", 
+                      "pandas=1.3.5", 
+                      "scipy=1.8.0",
+                      "igraph=0.10.6",
+                      "louvain=0.8.0"],
                       entry_points={
                           'console_scripts':[
                               'ema=ema.main:main'
