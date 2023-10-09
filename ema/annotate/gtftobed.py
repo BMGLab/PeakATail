e4b959b736198bd06d4bf3f01e080cf68c4fceff
Trex <amiramiritabat01@gmail.com>
2023-10-09 14:16:02 +0300

    ADD annotation feature directory and gtftobed

---
+++ ema/annotate/gtftobed.py
@@ -0,0 +1,43 @@
+def gtf_bed(endbeddir:str,
+             biotypes:tuple, 
+             source_tuple:tuple,
+             id_head:tuple,
+             gtfdir:str,
+             chro_index=1,
+             source_index=2,
+             start_index=3,
+             end_index=4,
+             strand_index=6,
+             id_index=9,
+             biotype_index=17,
+             score=0
+
+             ) -> None:
+
+    with open(gtfdir, "r") as gtf, open(endbeddir, "w") as endbed:
+        #ignore tuple
+        ignore = "#"
+        #index of id and biotype in gtf file format
+
+        for line in gtf:
+
+            if line.startswith(ignore):
+                continue
+
+            info = line.split()
+            # get necessary data from gtf line
+            # ex : suorce = "gene", id = "ENSG", gene_biotype = "protein_coding" 
+            source, id, gene_biotype = info[source_index], info[id_index].replace('"','').replace(";",'')[:4], info[biotype_index].replace('"','').replace(";",'')
+
+            if source in source_tuple and id in id_head and gene_biotype in biotypes:
+                chro, start, end, strand = info[chro_index], int(info[start_index]), int(info[end_index]), info[strand_index]
+
+                match strand:
+                    case "+":
+                        start = end - 1
+                    
+                    case "-":
+                        end = start + 1
+
+                bed_info = f"{chro}\t{start}\t{end}\t{id}\t{score}\t{strand}\n"
+                endbed.write(bed_info)
