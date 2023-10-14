6aea758b127bf4bade190d0e1b2e4d91c5f9ff3c
Trex <amiramiritabat01@gmail.com>
2023-10-14 17:49:43 +0300

    ADD make features file to gtftobed function

--- ema/annotate/gtftobed.py
+++ ema/annotate/gtftobed.py
@@ -3,6 +3,7 @@ def gtf_bed(endbeddir:str,
              source_tuple:tuple,
              id_head:tuple,
              gtfdir:str,
+             featuresdir:str,
              chro_index=1,
              source_index=2,
              start_index=3,
@@ -10,10 +11,11 @@ def gtf_bed(endbeddir:str,
              strand_index=6,
              id_index=9,
              biotype_index=17,
+             symbol_index=19,
              score=0
              ) -> None:
 
-    with open(gtfdir, "r") as gtf, open(endbeddir, "w") as endbed:
+    with open(gtfdir, "r") as gtf, open(endbeddir, "w") as endbed, open(featuresdir, "w") as features:
         #ignore tuple
         ignore = "#"
         #index of id and biotype in gtf file format
@@ -29,7 +31,7 @@ def gtf_bed(endbeddir:str,
             source, id, gene_biotype = info[source_index], info[id_index].replace('"','').replace(";",'')[:4], info[biotype_index].replace('"','').replace(";",'')
 
             if source in source_tuple and id in id_head and gene_biotype in biotypes:
-                chro, start, end, strand = info[chro_index], int(info[start_index]), int(info[end_index]), info[strand_index]
+                chro, start, end, strand, symbol = info[chro_index], int(info[start_index]), int(info[end_index]), info[strand_index], info[symbol_index]
 
                 match strand:
                     case "+":
@@ -39,7 +41,10 @@ def gtf_bed(endbeddir:str,
                         end = start + 1
 
                 bed_info = f"{chro}\t{start}\t{end}\t{id}\t{score}\t{strand}\n"
-                endbed.write(bed_info)
+                endbed.write(bed_info)\
+                
+                feature = f"{id}\t{symbol}\n"
+                features.write(feature)
 
 if __name__ == "__main__":
     gtf_bed()
