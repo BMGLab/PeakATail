f7eeae7577eaba02e6307050c7a6e6d4d85bf14a
Trex <amiramiritabat01@gmail.com>
2023-10-17 01:21:45 +0300

    debug find_close.py gtftobed.py

--- ema/annotate/find_close.py
+++ ema/annotate/find_close.py
@@ -14,18 +14,18 @@ def find_close(posbed_dir:str,
     #TODO this 5 line will be other module 
     posbed = pybedtools.BedTool(posbed_dir)
     negbed = pybedtools.BedTool(negbe_dir)
-    genomebed = pybedtools.BedTool(genomebed_dir)
+    genomebed = pybedtools.BedTool(genomebed_dir).sort()
 
     pasbed = negbed.cat(posbed, postmerge=False).sort()
     pasbed.saveas(mergebed)
 
-    annotaded = pasbed.closet(genomebed, D="ref", t="first", s=True)
+    annotaded = pasbed.closest(genomebed, D="ref", t="first", s=True)
     annotaded.saveas(annotatedbed_dir)
 
     annotated_frame = pd.read_csv(annotatedbed_dir, delimiter="\t", header=None)
-    genes_frame = annotated_frame[:,[pas_col,gene_col]]
-    genes_frame.sort_values(by=0, kind="quicksort")
-    genes_frame.to_csv(features, sep="\t", index=False)
+    genes_frame = annotated_frame.iloc[:,[pas_col,gene_col]]
+    genes_frame = genes_frame.sort_values(by=pas_col, kind="quicksort")
+    genes_frame.to_csv(features, sep="\t", index=False, header=False)
     return genes_frame
 
 if __name__ == "__main__":
--- ema/annotate/gtftobed.py
+++ ema/annotate/gtftobed.py
@@ -1,24 +1,24 @@
 def gtf_bed(endbeddir:str,
-             biotypes:tuple, 
-             source_tuple:tuple,
-             id_head:tuple,
              gtfdir:str,
              featuresdir:str,
-             chro_index=1,
+             biotypes=("protein_coding"), 
+             source_tuple=("gene"),
+             id_head=("ENSG"),
+             chro_index=0,
              source_index=2,
              start_index=3,
              end_index=4,
              strand_index=6,
              id_index=9,
              biotype_index=17,
-             symbol_index=19,
+             symbol_index=13,
              score=0
              ) -> None:
 
     with open(gtfdir, "r") as gtf, open(endbeddir, "w") as endbed, open(featuresdir, "w") as features:
         #ignore tuple
         ignore = "#"
-        #index of id and biotype in gtf file format
+        
 
         for line in gtf:
 
@@ -28,10 +28,10 @@ def gtf_bed(endbeddir:str,
             info = line.split()
             # get necessary data from gtf line
             # ex : suorce = "gene", id = "ENSG", gene_biotype = "protein_coding" 
-            source, id, gene_biotype = info[source_index], info[id_index].replace('"','').replace(";",'')[:4], info[biotype_index].replace('"','').replace(";",'')
+            source, id, gene_biotype = info[source_index], info[id_index].replace('"','').replace(";",''), info[biotype_index].replace('"','').replace(";",'')
 
-            if source in source_tuple and id in id_head and gene_biotype in biotypes:
-                chro, start, end, strand, symbol = info[chro_index], int(info[start_index]), int(info[end_index]), info[strand_index], info[symbol_index]
+            if source in source_tuple and id[:4] in id_head and gene_biotype in biotypes:
+                chro, start, end, strand, symbol = info[chro_index], int(info[start_index]), int(info[end_index]), info[strand_index], info[symbol_index].replace('"','').replace(";",'')
 
                 match strand:
                     case "+":
