d86ab43e12f51f696b6146441f85d8035f2f6b2c
Trex <amiramiritabat01@gmail.com>
2023-10-22 20:00:14 +0300

    make import dir complete

--- test/test_peak.py
+++ test/test_peak.py
@@ -1,6 +1,6 @@
-from ema import peak_calling, directory_config
+from ema.countmatrix.peackcalling import peak_calling, directory_config
 from ema.countmatrix.indexing import cb_total
 peak_calling(True, bedfilepath=directory_config.negbed, matrixpath=directory_config.negmatrixpath)
 peak_calling(False, bedfilepath=directory_config.posbed, matrixpath=directory_config.posmatrixpath)
-sve = open("cbtotoal.py", "w")
+sve = open("cbtotal.py", "w")
 sve.write(f"cb_dic = {cb_total}")
