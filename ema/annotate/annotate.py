c8b1a22a3faa557e14e6c6064d00f78f1a752421
Trex <amiramiritabat01@gmail.com>
2023-11-13 23:42:28 +0300

    DEBUG: filteredmatrix
    - remove none annotated pas from filteres matrix

--- ema/annotate/annotate.py
+++ ema/annotate/annotate.py
@@ -1,7 +1,12 @@
-from ema.config import directory_config
+from ema.config import directory_config, variable_config
 import pandas as pd
+import scipy.sparse as sp
+from scipy.io import mmwrite
 
-def annotate(countmatrix:pd.DataFrame(), genes:pd.DataFrame()) -> pd.DataFrame():
+def annotate(countmatrix:pd.DataFrame(), 
+            genes:pd.DataFrame(), 
+            annotated_matrix= directory_config.filterd_matrix
+            ) -> pd.DataFrame():
 
     # set row names as index number
     countmatrix.index = range(1, len(countmatrix) + 1)
@@ -12,6 +17,8 @@ def annotate(countmatrix:pd.DataFrame(), genes:pd.DataFrame()) -> pd.DataFrame()
     ann_gene.to_csv(directory_config.pas_geneid, index=True, header=False, sep="\t")
     # remove gene_id column 
     annotated_frame = annotated_frame.drop(annotated_frame.columns[0], axis=1)
+    sparse_matrix = sp.csc_matrix(annotated_frame.values)
+    mmwrite(target=annotated_matrix, a=sparse_matrix, comment=variable_config.matrixmarketheader)
 
     return annotated_frame
 
