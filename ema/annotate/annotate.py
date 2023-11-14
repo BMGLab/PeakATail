a30687de99c79ad76da10a4421bdb01fe1d4827c
Trex <amiramiritabat01@gmail.com>
2023-11-14 23:14:08 +0300

    DEBUG filter pas_gene

--- ema/annotate/annotate.py
+++ ema/annotate/annotate.py
@@ -5,7 +5,8 @@ from scipy.io import mmwrite
 
 def annotate(countmatrix:pd.DataFrame(), 
             genes:pd.DataFrame(), 
-            annotated_matrix= directory_config.filterd_matrix
+            annotated_matrix= directory_config.filterd_matrix,
+            pasbed_dir = directory_config.pasbed,
             ) -> pd.DataFrame():
 
     # set row names as index number
@@ -17,9 +18,13 @@ def annotate(countmatrix:pd.DataFrame(),
     ann_gene.to_csv(directory_config.pas_geneid, index=True, header=False, sep="\t")
     # remove gene_id column 
     annotated_frame = annotated_frame.drop(annotated_frame.columns[0], axis=1)
+    #save new sparse matrix
     sparse_matrix = sp.csc_matrix(annotated_frame.values)
     mmwrite(target=annotated_matrix, a=sparse_matrix, comment=variable_config.matrixmarketheader)
-
+    #save new pasbed file
+    pas_df = pd.read_csv(pasbed_dir, sep="\t", header=None)
+    filtered_pas = pas_df[pas_df[3].isin(annotated_frame.index)]
+    filtered_pas.to_csv(pasbed_dir, sep="\t", header=False, index=False)
     return annotated_frame
 
 if __name__ == "__main__":
