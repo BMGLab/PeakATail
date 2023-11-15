f36662a98f1c85bf3efcbd346c66810b04ea4792
yasinkaymaz <yasinkaymaz@gmail.com>
2023-11-15 19:46:03 +0300

    Small changes in the cl help

--- ema/cli.py
+++ ema/cli.py
@@ -4,18 +4,19 @@ def cli():
 
     parser = argparse.ArgumentParser(prog="ema")
 
-    parser.add_argument("--bamDir", dest="bam_dir", type=str, required=True,
-                        help="directory of bamfile examplebam.bam")
+    parser.add_argument("--bamFile", dest="bam_dir", type=str, required=True,
+                        help="a sorted bam file (an output of CellRanger or Star Solo)")
     print("bam")
 
-    parser.add_argument("--sequenceLen", dest="seqlen", type=int, required=True)
+    parser.add_argument("--sequenceLen", dest="seqlen", type=int, required=True,
+                        help="the sequencing read length (in *_2.fastq)")
     print("seq")
 
     parser.add_argument("--CellBarcodeLen", dest="cb_len", type=int, required=True)
 
     parser.add_argument("--BarcodeTag", type=str, dest="barcode_tag", required=True)
 
-    parser.add_argument("--gtfDir", dest="gtf_dir", type=str, required=True)
+    parser.add_argument("--gtfFile", dest="gtf_dir", type=str, required=True)
     return parser.parse_args()
     
     
