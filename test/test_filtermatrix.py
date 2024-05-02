368bfdc79e54d06ae9f2b0cf6f7dd29a91e7245e
Trex <amiramiritabat01@gmail.com>
2024-05-02 15:34:53 +0300

    change Clustering directory

--- test/test_filtermatrix.py
+++ test/test_filtermatrix.py
@@ -1,5 +1,5 @@
 from ema.matrixfilter import filter_cb, make_dataframe, preprocessing, filtered_cb_list
-from ema.clustering import clustering
+from ema.clustering.clustering import clustering
 filter_cb()
 
 print("cb ok")
