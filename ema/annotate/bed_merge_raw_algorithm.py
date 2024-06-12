0a6af15adf06511f36418d8dc62aa821d41ca9a7
Elyar-Bafandeh <bafandehelyar@gmail.com>
2024-06-12 13:14:46 +0300

    code repetition and variable names fixed in bed_merge_raw_algorithm.py

--- ema/annotate/bed_merge_raw_algorithm.py
+++ ema/annotate/bed_merge_raw_algorithm.py
@@ -13,16 +13,20 @@ def bed_merge(bed_file= dc.endbed,
     bed = open(bed_file)
     output_bed = open(output_bed_file , "w")
     prev_chro = "0"
-    string_start = 0
-    string_end =0
-    id_list = []
-    symbol_list = []
-    strand_list = []
-    def listToString(myList):
+    string_start , string_end = 0 , 0
+    id_list , symbol_list , strand_list= [],[],[]
+
+    def list_to_string(myList):
         resultStr = ""
         for x in myList:
             resultStr += x + ","
         return resultStr[:-1]
+    
+    def write_to_output(bed,record,string_start ,string_end  , id_list , symbol_list , strand_list , start = 0 , end = 0):
+        bed.write(record)
+        string_start = start
+        string_end = end
+        id_list , symbol_list , strand_list= [],[],[]
 
     for record in bed:
         split_record = record.split()
@@ -40,21 +44,14 @@ def bed_merge(bed_file= dc.endbed,
                     string_end = end
                 
             else:
-
-                output_bed.write(f"{chro}\t{string_start}\t{string_end}\t{listToString(id_list)}\t{listToString(symbol_list)}\t{listToString(strand_list)}\n")
-                string_start = start
-                string_end = end
-                id_list = []
-                symbol_list = []
-                strand_list = []
+                output_record = f"{chro}\t{string_start}\t{string_end}\t{list_to_string(id_list)}\t{list_to_string(symbol_list)}\t{list_to_string(strand_list)}\n"
+                write_to_output(output_bed,output_record,string_start ,string_end  , id_list , symbol_list , strand_list , start, end)
+                
             
         else:
-            output_bed.write(f"{chro}\t{string_start}\t{string_end}\t{listToString(id_list)}\t{listToString(symbol_list)}\t{listToString(strand_list)}\n")
-            string_start = 0
-            string_end =0
-            id_list = []
-            symbol_list = []
-            strand_list = []
+            output_record = f"{chro}\t{string_start}\t{string_end}\t{list_to_string(id_list)}\t{list_to_string(symbol_list)}\t{list_to_string(strand_list)}\n"
+            write_to_output(output_bed,output_record,string_start ,string_end  , id_list , symbol_list , strand_list)
+                
         prev_chro = chro
 
     
