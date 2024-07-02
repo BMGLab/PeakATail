cec0628b94295096f51b1f0c12dd35cfa63c4c81
Trex <amiramiritabat01@gmail.com>
2024-07-02 23:33:55 +0300

    ADD basic readme

--- test/test_peak.py
+++ test/test_peak.py
@@ -1,6 +1,6 @@
-from ema.countmatrix.peackcalling import peak_calling, directory_config
+from ema.countmatrix.peackcalling import peak_calling
 from ema.countmatrix.indexing import cb_total
-peak_calling(True, bedfilepath=directory_config.negbed, matrixpath=directory_config.negmatrixpath)
-peak_calling(False, bedfilepath=directory_config.posbed, matrixpath=directory_config.posmatrixpath)
+peak_calling(True, bedfilepath="negbed.bed", matrixpath="negmatrix.mtx")
+peak_calling(False, bedfilepath="posbed.bed", matrixpath="posmatrix.mtx")
 sve = open("cbtotal.py", "w")
 sve.write(f"cb_dic = {cb_total}")
