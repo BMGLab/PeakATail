9743197d79ff6c53e3ab0779da7672b429d085c7
Elyar-Bafandeh <bafandehelyar@gmail.com>
2024-07-03 14:51:23 +0300

    implement multiprocessing to ratio_score

--- ema/annotate/ratio_score.py
+++ ema/annotate/ratio_score.py
@@ -1,20 +1,35 @@
 
 import pandas as pd
-
+from multiprocessing import Pool
 import random
-def ratio_score(bed_file):#TODO change the parameter from bed to dataframe
+def ratio_score(bed_file , result_dir):#TODO change the parameter from bed to dataframe
     df = pd.read_csv(bed_file, sep='\t', header=None)
-
     grouped = df.groupby(5)#TODO add .filter
-    for key, group in grouped:
-        if len(group) > 1:
-            continue
-        print(f"Group '{key}':")
-        print(group)
-        group_c = (((group[1] + group[2])/2) - group[8])
-        print(group_c)
+    with Pool() as p:
+        all_results = p.starmap(process_pas_group, 
+                        [(group) for key, group in grouped])
         
-        break
+    with open(result_dir, 'w') as f:
+        f.write(''.join(sum(all_results, [])))
+
+
+def process_pas_group(group):
+    result = []
+    if len(group) < 1:
+        return#TODO add .filter to tario_score
+    group_distance = (((group[1] + group[2])/2) - group[8])
+    group_c = group_distance.sum()
+    group_ratio_scores = group_distance / group_c
+    print(group_ratio_scores)
+    group['group_ratio_scores'] = group_ratio_scores
+    for x in range(len(group)):
+        row_as_string = '\t'.join(group.iloc[x].astype(str))
+        result.append(row_as_string)
+
+
+        
+    return result
+
 
 
 
@@ -43,7 +58,7 @@ def add_random_gene_range_to_example_data(bed_file_input , bed_file_output):
       
         
 
-ratio_score(r"example_bed_2.bed")
+ratio_score(r"example_bed_2.bed" , r"result.bed")
 #add_random_coverage_to_example_data1(r"example_bed.bed" , r"example_bed_2.bed")
 
 
